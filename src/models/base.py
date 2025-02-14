c816453cd8bcf5d731110df1e9ba454544727d05
Aaron Glasenapp <glassbead@gmail.com>
2025-02-14 13:10:47 -0700

    tooling and env changes

--- src/models/base.py
+++ src/models/base.py
@@ -8,11 +8,11 @@ class Base(DeclarativeBase):
     pass
 
 def get_engine():
-    db_user = os.getenv('DB_USER')
-    db_password = os.getenv('DB_PASSWORD')
-    db_host = os.getenv('DB_HOST', 'climb_wise-db-1')
-    db_port = os.getenv('DB_PORT', 5432)
-    db_name = 'climb_wise_local_v2' # os.getenv('DB_NAME', 'climb_wise_local_v2')
+    db_user = os.getenv('POSTGRES_USER')
+    db_password = os.getenv('POSTGRES_PASSWORD')
+    db_host = os.getenv('POSTGRES_HOST', 'climb_wise-db-1')
+    db_port = os.getenv('POSTGRES_PORT', 5432)
+    db_name = os.getenv('POSTGRES_NAME', 'climb_wise_local_v2')
     db_url = f"postgresql://{db_user}:{db_password}@{db_host}:{db_port}/{db_name}"
     return create_engine(db_url)
 
