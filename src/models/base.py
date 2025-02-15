3618309ea9c300ecaa2e4cd0086d18f2edc41879
Aaron Glasenapp <glassbead@gmail.com>
2025-02-14 17:53:16 -0700

    update moved notebooks

--- src/models/base.py
+++ src/models/base.py
@@ -7,19 +7,19 @@ load_dotenv()
 class Base(DeclarativeBase):
     pass
 
-def get_engine():
+def get_engine(db_name):
     db_user = os.getenv('POSTGRES_USER')
     db_password = os.getenv('POSTGRES_PASSWORD')
     db_host = os.getenv('POSTGRES_HOST', 'climb_wise-db-1')
     db_port = os.getenv('POSTGRES_PORT', 5432)
-    db_name = os.getenv('POSTGRES_NAME', 'climb_wise_local_v2')
+    db_name = db_name
     db_url = f"postgresql://{db_user}:{db_password}@{db_host}:{db_port}/{db_name}"
     return create_engine(db_url)
 
 def get_sessionmaker(engine):
     return sessionmaker(bind=engine)
 
-def get_conn():
-    engine = get_engine()
+def get_conn(db_name=os.getenv('POSTGRES_NAME', 'climb_wise_local_v2')):
+    engine = get_engine(db_name)
     Session = get_sessionmaker(engine)
     return engine, Session
